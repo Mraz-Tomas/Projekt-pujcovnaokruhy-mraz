3767f482e7ed0a99c8588efef626e2b5fb2cc9b8
Tomino16 <mraz.tomas16@email.cz>
2026-01-12 11:41:18 +0100

    entities

---
+++ src/okruhy-pujcovna/okruhy-pujcovnaMVCAPP/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using okruhy_pujcovnaMVCAPP.Entities;
+using okruhy_pujcovnaMVCAPP.Models;
+using System.Reflection.PortableExecutable;
+using Microsoft.EntityFrameworkCore;
+
+namespace okruhy_pujcovnaMVCAPP.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Circuit> Circuits { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<CircuitCar> CircuitCars { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySQL(
+                "server=localhost;database=4c2_mraztomas_db2;user=mraztomas;password=123456;");
+        }
+        
+    }
+}
--- src/okruhy-pujcovna/okruhy-pujcovnaMVCAPP/Entities/car.cs
+++ src/okruhy-pujcovna/okruhy-pujcovnaMVCAPP/Entities/car.cs
@@ -1,11 +1,23 @@
-﻿namespace okruhy_pujcovnaMVCAPP.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace okruhy_pujcovnaMVCAPP.Entities
 {
+    [Table("car")]
     public class car
     {
+        [Key]
+        [Column("CarId")]
         public int Carid { get; set; }
+        [Column("Brand")]
         public string Brand { get; set; }
+        [Column("Model")]
         public string Model { get; set; }
+        [Column("PowerHp")]
         public int PowerHp { get; set; }
+        [Column("PricePerDay")]
         public decimal priceperday { get; set; }
+
+        public ICollection<circuitcar> CircuitCars { get; set; }
     }
 }
