b1927cdc54c4ba1b531ef6ced52a1ebcbaf37222
Tomino16 <mraztomas@sssvt.cz>
2026-03-15 23:18:51 +0100

    vse hotove

---
+++ okruhy-pujcovnaMVCAPP/Controllers/ReservationsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using okruhy_pujcovnaMVCAPP.Data;
+using okruhy_pujcovnaMVCAPP.Entities;
+using System.Linq;
+
+public class ReservationsController : Controller
+{
+    AppDbContext DbContext = new AppDbContext();
+
+    public IActionResult SelectCar(int circuitId)
+    {
+        var cars = DbContext.CircuitCars
+            .Where(x => x.circuit_id == circuitId && x.is_available)
+            .Select(x => x.Car)
+            .ToList();
+
+        ViewBag.CircuitId = circuitId;
+
+        return View(cars);
+    }
+
+    public IActionResult Create(int carId, int circuitId)
+    {
+        ViewBag.CarId = carId;
+        ViewBag.CircuitId = circuitId;
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(int carId, int circuitId, DateTime startDate, DateTime endDate)
+    {
+        int userId = int.Parse(User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier).Value);
+
+        var r = new reservation
+        {
+            user_id = userId,
+            car_id = carId,
+            circuit_id = circuitId,
+            start_date = startDate,
+            end_date = endDate
+        };
+
+        DbContext.Reservations.Add(r);
+        DbContext.SaveChanges();
+
+        TempData["success"] = "Rezervace byla uspesne vytvorena!";
+
+        return RedirectToAction("Index", "Home");
+    }
+}
--- okruhy-pujcovnaMVCAPP/Entities/reservation.cs
+++ okruhy-pujcovnaMVCAPP/Entities/reservation.cs
@@ -4,25 +4,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace okruhy_pujcovnaMVCAPP.Entities
 {
-    [Table("reservation")]
+    [Table("reservations")]
     public class reservation
     {
         [Key]
-        [Column("reservation_id")]
         public int reservation_id { get; set; }
-        [Column("user_id")]
+
         public int user_id { get; set; }
-        public user user { get; set; }
 
-        [Column("circuit_car_id")]
-        public int circuit_car_id { get; set; }
-        public circuitcar Circuitcar { get; set; }
+        public int car_id { get; set; }
 
-        [Column("from_date")]
-        public DateTime from_date { get; set; }
-        [Column("to_date")]
-        public DateTime to_date { get; set; }
-        [Column("total_price")]
-        public decimal total_price { get; set; }
+        public int circuit_id { get; set; }
+
+        public DateTime start_date { get; set; }
+
+        public DateTime end_date { get; set; }
     }
 }
